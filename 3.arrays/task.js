db0f3f0c97963ebe01292e886366a44aea7f8cc0
Коля <kolya0163@mail.ru>
2023-07-09 23:40:47 +0400

    выполнено 3 дз

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
+    let b = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])
+    return (b);
+  }
   
-}
-
-function getUsersNamesInAgeRange(users, gender) {
+  function getUsersNamesInAgeRange(user, gender) {
+    return user.filter(user => user.gender === gender)
+    .map(filteredUser => filteredUser.age)
+    .reduce(
+      (accumulator, age, index, aserAgesArray) => accumulator + age / aserAgesArray.length, 0
+      );
+  }
   
-}
