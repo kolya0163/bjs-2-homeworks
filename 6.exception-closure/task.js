20f64f281d8175eb53bfa061fa31af87729a0520
Коля <kolya0163@mail.ru>
2023-07-23 12:12:32 +0400

    1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,56 @@
-﻿
+﻿function parseCount (value) {
+    const number = Number.parseFloat(value);
+    if (isNaN (number)) {
+        throw new Error ('Невалидное значение');        
+    } 
+    return number;
+}     
+
+function validateCount (value) {
+    try {
+        return parseCount (value);
+    }
+    catch (err) {
+        return err;
+    }
+}
+
+
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    
+        if (a + b < c || a + c < b || b + c < a) {
+           throw new Error ('Треугольник с такими сторонами не существует');
+        }       
+    }
+
+
+    get perimeter() {
+        return (this.a + this.b + this.c);
+    }
+
+    get area() {
+        let p = this.perimeter / 2;
+        let ar = Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+        return ar;
+    }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует"
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует"
+      }
+    }
+  }
+}
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,49 @@
-//Задача № 1
+//Задача 1:
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  return function (...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find(
+      (objectInCache) => objectInCache.hash === hash
+    );
+
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    }
+
+    let result = func(...args);
+    cache.push({ hash, result });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  };
 }
 
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
   
+  function debounceDecoratorNew(func, delay){
+    let timerId = null;
+  function resultFunction(...args) {
+    resultFunction.allCount++;
+
+    if(!timerId){
+      func(...args);
+      resultFunction.count++;
+    }
+
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+    func(...args);
+    resultFunction.count++;
+    },delay)
+  }
+  resultFunction.count = 0;
+  resultFunction.allCount = 0;  
+  return resultFunction;
 }
+
