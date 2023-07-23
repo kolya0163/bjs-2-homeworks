b586bd659f6aa16a4bd90d3a07b1e39ec85569e5
Коля <kolya0163@mail.ru>
2023-07-23 15:15:05 +0400

    3

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,22 +1,22 @@
 //Задача 1:
 function cachingDecoratorNew(func) {
-	let cache = {}
-	return (...args) => {
-		const hash = md5(args);
-		if (hash in cache) {
-			return 'Из кеша: ' + cache[hash];
-		}
-		const result = func(...args);
-		if (Object.keys(cache).length > 4) {
-			cache[hash] = result;
-			let extraElem = Object.keys(cache)[0];
-			delete cache[extraElem];
-			return ' Вычисляем: ' + result;
-		}
-		cache[hash] = result;
-		return ' Вычисляем: ' + result;
-	}
-}
+  let cache = {}
+  return (...args) => {
+   const hash = md5(args);
+   if (hash in cache) {
+    return 'Из кеша: ' + cache[hash];
+   }
+   const result = func(...args);
+   if (Object.keys(cache).length > 4) {
+    cache[hash] = result;
+    let extraElem = Object.keys(cache)[0];
+    delete cache[extraElem];
+    return ' Вычисляем: ' + result;
+   }
+   cache[hash] = result;
+   return ' Вычисляем: ' + result;
+  }
+ }
 //Задача № 2
   
 function debounceDecoratorNew(func, delay){
